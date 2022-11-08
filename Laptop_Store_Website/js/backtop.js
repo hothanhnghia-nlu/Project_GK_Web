fe1e102ea34af736d9402ad7bb132c9dd1b1c00c
Bùi Quốc Long <buiquoclongkt123@gmail.com>
2022-11-08 22:01:40 +0700

    Update backtop

--- Laptop_Store_Website/js/backtop.js
+++ Laptop_Store_Website/js/backtop.js
@@ -3,20 +3,15 @@
         scrollFunction()}
 
         function scrollFunction() {
-            if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 300) {
-                 $('#back_top').fadeIn();
-                back_top.classList.add('back_top-open');
-
+            if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+                backtop.classList.add('backtop-open');
             } else {
-                $('#back_top').fadeOut();
-                back_top.classList.remove('back_top-open');
+                backtop.classList.remove('backtop-open');
             }
         }
-
-    $('#back_top').click(function (){
+    $('#backtop').click(function (){
         $('html,body').animate({
             scrollTop : 0
         },1000);
     })
-
 })(jQuery);
