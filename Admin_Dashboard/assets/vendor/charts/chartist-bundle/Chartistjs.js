6680491f81e1fef6f0cb774b879ed81c4ab01e40
Ho Thanh Nghia <20130333@st.hcmuaf.edu.vn>
2022-11-13 10:08:11 +0700

    Update html and js

--- Admin_Dashboard/assets/vendor/charts/chartist-bundle/Chartistjs.js
+++ Admin_Dashboard/assets/vendor/charts/chartist-bundle/Chartistjs.js
@@ -491,7 +491,7 @@
 
         if ($('.ct-chart-horizontal').length) {
             new Chartist.Bar('.ct-chart-horizontal', {
-                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                labels: ['MSI', 'Lenovo', 'Dell', 'Asus', 'Acer', 'HP', 'Apple'],
                 series: [
                     [5, 4, 3, 7, 5, 10, 3],
                     [3, 2, 9, 5, 4, 6, 4]
